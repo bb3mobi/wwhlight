a5741eaf5908858fdd501aa337879494a98b6dc8
unknown <C:\Program Files\The Bat!\Mail>
2016-03-30 20:15:21 +0600

    Add events

--- event/listener.php
+++ event/listener.php
@@ -5,7 +5,7 @@
  * @copyright (c) 2015 alg 
  * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
  *
- */
+*/
 
 namespace alg\wwhlight\event;
 
@@ -16,14 +16,13 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class listener implements EventSubscriberInterface
 {
+	private $counter_users_total = 0;
 
-	public function __construct(\phpbb\config\config $config
-									, \phpbb\template\template $template
-									, \phpbb\user $user
-									, \phpbb\db\driver\driver_interface $db
-									, $phpbb_root_path, $php_ext
-									, \phpbb\auth\auth $auth
-								)
+	private $counter_users_reg = 0;
+
+	private $counter_users_hidden = 0;
+
+	public function __construct(\phpbb\config\config $config, \phpbb\template\template $template, \phpbb\user $user, \phpbb\db\driver\driver_interface $db, $phpbb_root_path, $php_ext, \phpbb\auth\auth $auth)
 	{
 		$this->template = $template;
 		$this->user = $user;
@@ -32,10 +31,6 @@ class listener implements EventSubscriberInterface
 		$this->phpbb_root_path = $phpbb_root_path;
 		$this->php_ext = $php_ext;
 		$this->auth = $auth;
-		$this->counter_users_total = 0;
-		$this->counter_users_reg = 0;
-		$this->counter_users_hidden = 0;
-
 	}
 
 	static public function getSubscribedEvents()
@@ -44,53 +39,85 @@ class listener implements EventSubscriberInterface
 			'core.index_modify_page_title'	=> 'index_modify_page_title',
 		);
 	}
-	 public function index_modify_page_title()
+
+	public function index_modify_page_title()
 	{
-		  $this->user->add_lang_ext('alg/wwhlight', 'wwhlight');
-		$username_full = '';
-		  $users_list = '';
+		global $phpbb_dispatcher;
 
-		$sql = "SELECT user_id, username, username_clean, user_colour, user_type, user_ip, user_lastvisit, user_allow_viewonline  " .
-					" FROM  " . USERS_TABLE . 
-						" WHERE user_type <>" . USER_IGNORE . 
-						" AND DATE(FROM_UNIXTIME (user_lastvisit)) = CURDATE()" . 
-						" ORDER BY user_lastvisit desc";
-		$result = $this->db->sql_query($sql);
-		while ($row = $this->db->sql_fetchrow($result))
+		$this->user->add_lang_ext('alg/wwhlight', 'wwhlight');
+
+		$user_online_link = $rowset = array();
+
+		$sql_ary = array(
+			'SELECT'	=> 'u.user_id, u.username, u.username_clean, u.user_type, u.user_colour, u.user_ip, u.user_lastvisit, u.user_allow_viewonline',
+			'FROM'		=> array(
+				USERS_TABLE	=> 'u',
+			),
+			'WHERE'		=> 'user_type <> ' . USER_IGNORE . ' AND DATE(FROM_UNIXTIME (user_lastvisit)) = CURDATE()',
+			'ORDER_BY'	=> 'u.user_lastvisit DESC',
+		);
+
+		/**
+		* Modify SQL query to obtain wwhlight users data
+		*
+		* @event wwhlight.obtain_users_online_string_sql
+		*/
+		$vars = array('sql_ary');
+		extract($phpbb_dispatcher->trigger_event('wwhlight.obtain_users_online_string_sql', compact($vars)));
+
+		$result = $this->db->sql_query($this->db->sql_build_query('SELECT', $sql_ary));
+		$rowset = $this->db->sql_fetchrowset($result);
+		$this->db->sql_freeresult($result);
+
+		foreach ($rowset as $row)
 		{
-				$username_full = get_username_string(( 'full'), $row['user_id'], $row['username'], $row['user_colour']);
-				$last_visit_time =  sprintf($this->user->lang['WWHLIGHT_LATEST'], $this->user->format_date($row['user_lastvisit'], 'H:i') );
-				$hover_info = $last_visit_time ? ' title="' . $last_visit_time . '"' : '';
-				if ($row['user_allow_viewonline'] )
+			$last_visit_time =  sprintf($this->user->lang['WWHLIGHT_LATEST'], $this->user->format_date($row['user_lastvisit'], 'H:i') );
+			$hover_info = $last_visit_time ? ' title="' . $last_visit_time . '"' : '';
+			if ($row['user_allow_viewonline'])
+			{
+				$user_online_link[$row['user_id']] = '<span' . $hover_info . '>' . get_username_string(($row['user_type'] <> USER_IGNORE) ? 'full' : 'no_profile', $row['user_id'], $row['username'], $row['user_colour']) . '</span>';
+				$this->counter_users_reg++;
+			}
+			else
+			{
+				if ($this->auth->acl_get('u_viewonline'))
 				{
-					$users_list .= $this->user->lang['COMMA_SEPARATOR'] . '<span' . $hover_info . '>' . $username_full . '</span>' ;
-					$this->counter_users_reg++;
+					$user_online_link[$row['user_id']] = '<em>' . $row['username'] . '</em>';
 				}
-				else
-				{
-					if ($this->auth->acl_get('u_viewonline'))
-					{
-						$users_list .= $this->user->lang['COMMA_SEPARATOR'] . '<em' . $hover_info . '>' .$username_full . '</em>' ;
-					}
-					$this->counter_users_hidden++;
-				}
-				$this->counter_users_total++;
+				$this->counter_users_hidden++;
+			}
+			$this->counter_users_total++;
 		}
+		$online_userlist = $this->user->lang['REGISTERED_USERS'] . ' ' . implode(', ', $user_online_link);
 
-		$users_list = utf8_substr($users_list, utf8_strlen($this->user->lang['COMMA_SEPARATOR']));
-		if ($users_list == '')
+		/**
+		* Modify wwhlight userlist data
+		*
+		* @event wwhlight.obtain_users_online_string_modify
+		* @var	array	rowset				Array with wwhlight users data
+		* @var	array	user_online_link	Array with wwhlight usernames
+		* @var	string	online_userlist		String containing users wwhlight list
+		*/
+		$vars = array(
+			'rowset',
+			'user_online_link',
+			'online_userlist',
+		);
+		extract($phpbb_dispatcher->trigger_event('wwhlight.obtain_users_online_string_modify', compact($vars)));
+
+		if (!sizeof($user_online_link))
 		{
-			$users_list = $this->user->lang['NO_ONLINE_USERS'];
+			$online_userlist = $this->user->lang['NO_ONLINE_USERS'];
 		}
 
 		$this->template->assign_vars(array(
-			'WWHLIGHT_LIST'		=> $this->user->lang['REGISTERED_USERS'] . ' ' . $users_list,
+			'WWHLIGHT_LIST'		=> $online_userlist,
 			'WWHLIGHT_DETAILS'	=> $this->obtain_users_string(),
 			'WWHLIGHT_RECORD'	=> sprintf($this->user->lang['WWHLIGHT_RECORD'], $this->config['wwhlight_record'], $this->config['wwhlight_record_time']) . '<br />',
 		));
 	}
-	 
-	 private function obtain_users_string()
+
+	private function obtain_users_string()
 	{
 		$total_users_string = $this->user->lang('WWHLIGHT_TOTAL', $this->counter_users_total);
 		$total_users_string .= $this->user->lang('WWHLIGHT_REG_USERS', $this->counter_users_reg);
